7d946f2f2c88019866af87877ddd0d11131d01de
Henrik Hellerøy <henrik.helleroy@bekk.no>
2014-02-21 17:35:15 +0100

    Removed OAuth stuff.

--- app/models/user.js
+++ app/models/user.js
@@ -1,35 +1,17 @@
 'use strict';
 
-/**
- * Module dependencies.
- */
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     crypto = require('crypto');
 
-/**
- * User Schema
- */
 var UserSchema = new Schema({
     name: String,
     email: String,
-    username: {
-        type: String,
-        unique: true
-    },
+    username: {type: String, unique: true},
     hashed_password: String,
-    provider: String,
-    salt: String,
-    facebook: {},
-    twitter: {},
-    github: {},
-    google: {},
-    linkedin: {}
+    salt: String
 });
 
-/**
- * Virtuals
- */
 UserSchema.virtual('password').set(function(password) {
     this._password = password;
     this.salt = this.makeSalt();
@@ -38,42 +20,27 @@ UserSchema.virtual('password').set(function(password) {
     return this._password;
 });
 
-/**
- * Validations
- */
 var validatePresenceOf = function(value) {
     return value && value.length;
 };
 
-// the below 4 validations only apply if you are signing up traditionally
 UserSchema.path('name').validate(function(name) {
-    // if you are authenticating by any of the oauth strategies, don't validate
-    if (!this.provider) return true;
     return (typeof name === 'string' && name.length > 0);
 }, 'Name cannot be blank');
 
 UserSchema.path('email').validate(function(email) {
-    // if you are authenticating by any of the oauth strategies, don't validate
-    if (!this.provider) return true;
     return (typeof email === 'string' && email.length > 0);
 }, 'Email cannot be blank');
 
 UserSchema.path('username').validate(function(username) {
-    // if you are authenticating by any of the oauth strategies, don't validate
-    if (!this.provider) return true;
     return (typeof username === 'string' && username.length > 0);
 }, 'Username cannot be blank');
 
 UserSchema.path('hashed_password').validate(function(hashed_password) {
-    // if you are authenticating by any of the oauth strategies, don't validate
-    if (!this.provider) return true;
     return (typeof hashed_password === 'string' && hashed_password.length > 0);
 }, 'Password cannot be blank');
 
 
-/**
- * Pre-save hook
- */
 UserSchema.pre('save', function(next) {
     if (!this.isNew) return next();
 
@@ -83,9 +50,6 @@ UserSchema.pre('save', function(next) {
         next();
 });
 
-/**
- * Methods
- */
 UserSchema.methods = {
     /**
      * Authenticate - check if the passwords are the same
