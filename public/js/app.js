dbb892f718ef8787c205fbfb6739f5c06a3b51e3
Henrik Hellerøy <helleroy@gmail.com>
2014-03-07 00:14:27 +0100

    Removed articles,

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,7 @@
 'use strict';
 
-angular.module('am', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'am.system', 'am.articles', 'am.residents', 'am.apartments']);
+angular.module('am', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'am.system', 'am.residents', 'am.apartments']);
 
 angular.module('am.system', []);
-angular.module('am.articles', []);
 angular.module('am.residents', []);
 angular.module('am.apartments', []);
--- public/js/config.js
+++ public/js/config.js
@@ -6,22 +6,6 @@ angular.module('am').config(['$stateProvider', '$urlRouterProvider',
         $urlRouterProvider.otherwise('/');
 
         $stateProvider
-            .state('all articles', {
-                url: '/articles',
-                templateUrl: 'views/articles/list.html'
-            })
-            .state('create article', {
-                url: '/articles/create',
-                templateUrl: 'views/articles/create.html'
-            })
-            .state('edit article', {
-                url: '/articles/:articleId/edit',
-                templateUrl: 'views/articles/edit.html'
-            })
-            .state('article by id', {
-                url: '/articles/:articleId',
-                templateUrl: 'views/articles/view.html'
-            })
             .state('all residents', {
                 url: '/residents',
                 templateUrl: 'views/residents/list.html'
