7dcbaed454521a84af7cd97e195af92336e75f21
Henrik Hellerøy <henrik.helleroy@bekk.no>
2014-03-06 17:02:19 +0100

    Update residents when saving, updating and removing apartments in backend.

--- app/controllers/apartments.js
+++ app/controllers/apartments.js
@@ -2,10 +2,39 @@
 
 var mongoose = require('mongoose'),
     Apartment = mongoose.model('Apartment'),
+    Resident = mongoose.model('Resident'),
     _ = require('lodash');
+
+var updateResident = function (err, resident, apartment) {
+    if (err) {
+        console.log(err);
+    } else {
+        resident.apartment = apartment;
+        resident.save();
+    }
+};
+
+var saveApartmentToResident = function (apartment) {
+    var update = function (err, resident) {
+        updateResident(err, resident, apartment);
+    };
+    for (var i in apartment.residents) {
+        Resident.load(apartment.residents[i]._id, update);
+    }
+};
+
+var removeApartmentFromResident = function (apartment) {
+    var update = function (err, resident) {
+        updateResident(err, resident, '');
+    };
+    for (var i in apartment.residents) {
+        Resident.load(apartment.residents[i]._id, update);
+    }
+};
 
 exports.create = function (req, res) {
     var apartment = new Apartment(req.body);
+    saveApartmentToResident(apartment);
     apartment.save();
     res.jsonp(apartment);
 };
@@ -36,6 +65,7 @@ exports.all = function (req, res) {
 exports.update = function (req, res) {
     var apartment = req.apartment;
     apartment = _.extend(apartment, req.body);
+    saveApartmentToResident(apartment);
     apartment.save(function () {
         res.jsonp(apartment);
     });
@@ -47,6 +77,7 @@ exports.destroy = function (req, res) {
         if (err) {
             res.render('error', {status: 500});
         } else {
+            removeApartmentFromResident(apartment);
             res.jsonp(1);
         }
     });
