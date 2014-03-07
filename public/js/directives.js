5835b8fbcb190275387a1e6a48c10c166829b46b
Henrik Hellerøy <henrik.helleroy@bekk.no>
2014-03-07 15:47:39 +0100

    Directives for resident, apartment and parkingspot.

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,1 +1,25 @@
 'use strict';
+
+angular.module('am').directive('resident', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/residents/resident.html',
+        scope: {}
+    };
+});
+
+angular.module('am').directive('apartment', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/apartments/apartment.html',
+        scope: {}
+    };
+});
+
+angular.module('am').directive('parkingspot', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/parkingspots/parkingspot.html',
+        scope: {}
+    };
+});
