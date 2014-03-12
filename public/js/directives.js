d5faf34f383dd8683798fac99e015fd2d382c1fd
Henrik Hellerøy <henrik.helleroy@bekk.no>
2014-03-12 16:57:02 +0100

    Cleaned up directives for resident, apartment and parkingspot.

--- public/js/directives.js
+++ public/js/directives.js
@@ -3,31 +3,26 @@
 angular.module('am').directive('resident', function () {
     return {
         restrict: 'E',
+        controller: 'ResidentsController',
         templateUrl: 'views/residents/resident.html',
-        scope: {
-            resident: '='
-        }
+        scope: {}
     };
 });
 
 angular.module('am').directive('apartment', function () {
     return {
         restrict: 'E',
+        controller: 'ApartmentsController',
         templateUrl: 'views/apartments/apartment.html',
-        scope: {
-            apartment: '=',
-            residents: '='
-        }
+        scope: {}
     };
 });
 
 angular.module('am').directive('parkingspot', function () {
     return {
         restrict: 'E',
+        controller: 'ParkingSpotsController',
         templateUrl: 'views/parkingspots/parkingspot.html',
-        scope: {
-            parkingspot: '=',
-            residents: '='
-        }
+        scope: {}
     };
 });
