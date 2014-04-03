d42e96b3dfc0a0f44f6b81e72cfcb63b09fe797e
Henrik Hellerøy <helleroy@gmail.com>
2014-04-03 23:06:24 +0200

    Removed directives for resident, apartment and parkingspot

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,12 +1,5 @@
 'use strict';
 
-angular.module('am').directive('resident', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'views/residents/resident.html',
-    };
-});
-
 angular.module('am').directive('residents', function () {
     return {
         restrict: 'E',
@@ -16,29 +9,11 @@ angular.module('am').directive('residents', function () {
     };
 });
 
-angular.module('am').directive('apartment', function () {
-    return {
-        restrict: 'E',
-        controller: 'ApartmentsController',
-        templateUrl: 'views/apartments/apartment.html',
-        scope: {}
-    };
-});
-
 angular.module('am').directive('apartments', function () {
     return {
         restrict: 'E',
         controller: 'ApartmentsController',
         templateUrl: 'views/apartments/list.html',
-        scope: {}
-    };
-});
-
-angular.module('am').directive('parkingspot', function () {
-    return {
-        restrict: 'E',
-        controller: 'ParkingSpotsController',
-        templateUrl: 'views/parkingspots/parkingspot.html',
         scope: {}
     };
 });
