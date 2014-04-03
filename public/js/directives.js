b53a8b31f3565b75ee2c4d1d415492b72ee120ec
Henrik Hellerøy <helleroy@gmail.com>
2014-04-03 22:51:42 +0200

    Directives for editing text fields inline.

--- public/js/directives.js
+++ public/js/directives.js
@@ -51,3 +51,54 @@ angular.module('am').directive('parkingspots', function () {
         scope: {}
     };
 });
+
+angular.module('am').directive('clickToEdit', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'views/directives/clicktoedit.html',
+        scope: {
+            value: '=editableModel'
+        },
+        controller: function ($scope) {
+            $scope.editorEnabled = false;
+            $scope.editableValue = $scope.value;
+
+            $scope.enableEditor = function () {
+                $scope.editorEnabled = true;
+                $scope.editableValue = $scope.value;
+            };
+
+            $scope.disableEditor = function () {
+                $scope.editorEnabled = false;
+            };
+
+            $scope.done = function () {
+                $scope.value = $scope.editableValue;
+                $scope.disableEditor();
+            };
+        }
+    };
+});
+
+angular.module('am').directive('toggleableLink', function () {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'views/directives/toggleablelink.html',
+        transclude: true,
+        scope: {
+            modelToWatch: '&',
+            clickFunction: '&'
+        },
+        link: function (scope) {
+            scope.showLink = false;
+            scope.$watch(scope.modelToWatch, function (newValue, oldValue) {
+                if (newValue === oldValue) {
+                    return;
+                }
+                scope.showLink = true;
+            }, true);
+        }
+    };
+});
